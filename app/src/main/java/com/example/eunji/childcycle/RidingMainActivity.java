feafdd1477cc65ed378289331625afba66dbceca
Eunji <zluoy@naver.com>
2016-09-25 01:38:41 +0900

    main xml 생성, RidingMainActivity 연결. 글자 dp->sp로 변경

---
+++ app/src/main/java/com/example/eunji/childcycle/RidingMainActivity.java
@@ -0,0 +1,15 @@
+package com.example.eunji.childcycle;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Eunji on 2016. 9. 25..
+ */
+public class RidingMainActivity extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+    }
+}
