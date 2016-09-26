89d783b1e33b1c32e33331fedd2750140ca51a2a
Eunji <zluoy@naver.com>
2016-09-26 16:42:10 +0900

    준비하는 화면 변경, 준비화면 하단 이미지버튼 추가

--- app/src/main/java/com/example/eunji/childcycle/PrepareFirstActivity.java
+++ app/src/main/java/com/example/eunji/childcycle/PrepareFirstActivity.java
@@ -5,24 +5,42 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 /**
  * Created by Eunji on 2016. 9. 11..
  */
-public class PrepareFirstActivity extends Activity {
+
+public class PrepareFirstActivity extends Activity{
+    int i=0;
+
+    String s[] = {"보호장구를 착용하세요", "자전거에 탑승하세요", "hihi!"};
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_prepare_first);
 
-        findViewById(R.id.button).setOnClickListener(ClickListener);
-    }
+        Button btn = (Button) findViewById(R.id.button);
 
-    Button.OnClickListener ClickListener = new View.OnClickListener() {
-        public void onClick(View v) {
-            Intent intent = new Intent(PrepareFirstActivity.this, PrepareSecondActivity.class);
-            startActivity(intent);
-        }
-    };
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                TextView txtview1 = (TextView) findViewById(R.id.text_change);
+
+                if((i%3)==0)
+                    txtview1.setText(s[0]);
+                else if((i%3)==1)
+                    txtview1.setText(s[1]);
+                else if((i%3)==2) {
+                    Intent intent = new Intent(getApplicationContext(), RidingMainActivity.class);
+                    startActivity(intent);
+                }
+
+                i++;
+            }
+        });
+
+    }
 
 }
