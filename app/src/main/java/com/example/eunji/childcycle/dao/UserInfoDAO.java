6cd20815f7233984dfd44e5b6de34ef585c725c8
hyesunzzang <24882499@naver.com>
2016-10-28 13:39:00 +0900

    create UserInfoDAO, rename package

---
+++ app/src/main/java/com/example/eunji/childcycle/dao/UserInfoDAO.java
@@ -0,0 +1,16 @@
+package com.example.eunji.childcycle.dao;
+
+import com.example.eunji.childcycle.dto.UserDTO;
+
+/**
+ * Created by choihyesun on 2016. 10. 14..
+ */
+
+public class UserInfoDAO {
+    private UserDTO userDTO;
+    private static final String TAG = "Hanium";
+
+    public void confirmUser(){
+
+    }
+}
