67c19fc588781c7b87dcede53f34a89c4fc1e021
Eunji <zluoy@naver.com>
2016-11-08 00:24:40 +0900

    FinishRidingActivity 를 RecordTableActivity로 분할함

--- app/src/main/java/com/example/eunji/childcycle/FinishRidingActivity.java
+++ app/src/main/java/com/example/eunji/childcycle/FinishRidingActivity.java
@@ -42,12 +42,13 @@ public class FinishRidingActivity extends AppCompatActivity{
     }
 
     public void historyClick(View v){
-        setContentView(R.layout.record_table);
+        Intent intent = new Intent(getApplicationContext(), RecordTableActivity.class);
+        startActivity(intent);
     }
 
 
-//    @Override
-//    public void onBackPressed() {
-//        //super.onBackPressed();
-//    }
+    @Override
+    public void onBackPressed() {
+        //super.onBackPressed();
+    }
 }
